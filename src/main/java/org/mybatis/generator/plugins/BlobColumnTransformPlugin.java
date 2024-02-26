021a8d30adec5b9435dfd6aac6ff003fbff7c347
Pparty_shuyongqiang <287424271@qq.com>
2024-02-26 10:36:16 +0800

    BlobColumnTransformPlugin 序列化成json字符串时，增加参数，使得 数字key序列化出来带""

--- src/main/java/org/mybatis/generator/plugins/BlobColumnTransformPlugin.java
+++ src/main/java/org/mybatis/generator/plugins/BlobColumnTransformPlugin.java
@@ -1,3 +1,18 @@
+/*
+ *    Copyright 2006-2024 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.mybatis.generator.plugins;
 
 import java.util.List;
@@ -125,7 +140,8 @@ public class BlobColumnTransformPlugin extends PluginAdapter {
 			if (isText) {
 				// 默认json string
 				body = String.format(
-						"return com.alibaba.fastjson.JSON.toJSONString(this.%s);", domainObjectFieldName);
+						"return com.alibaba.fastjson.JSON.toJSONString(this.%s, com.alibaba.fastjson.serializer.SerializerFeature.WriteNonStringKeyAsString);",
+						domainObjectFieldName);
 			} else {
 				// byte[]
 				body = String.format("return util.KryoUtils.serializeClassAndObjectWithVersion(this.%s);\r\n",
