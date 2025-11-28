7e37aa24d19e30b6ca2644afb28dd32e369d3c14
shuyongqiang <shuyongqiang@cyou-inc.com>
2025-11-28 17:59:11 +0800

    修正InsertOrUpdatePlugin

--- src/main/java/org/mybatis/generator/plugins/InsertOrUpdatePlugin.java
+++ src/main/java/org/mybatis/generator/plugins/InsertOrUpdatePlugin.java
@@ -1,84 +1,140 @@
-/*
- *    Copyright 2006-2024 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.mybatis.generator.plugins;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
-import org.mybatis.generator.api.dom.OutputUtilities;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.JavaVisibility;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.Parameter;
 import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.Document;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
-import org.mybatis.generator.codegen.mybatis3.ListUtilities;
 import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
 
-/**
- * 不存在就插入，存在就更新,官方文档不建议同时插多条数据，有潜在风险，暂时只实现单条。
- * 2024年3月11日 下午2:56:41
- * @author SYQ
- */
 public class InsertOrUpdatePlugin extends PluginAdapter {
+
 	@Override
-	public boolean validate(List<String> list) {
+	public boolean validate(List<String> warnings) {
 		return true;
 	}
 
+	/**
+	 * 1. 生成 Java 接口方法
+	 * 保持 abstract = true，确保生成 interface 方法时不带大括号
+	 */
 	@Override
-	public boolean sqlMapInsertElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+	public boolean clientGenerated(Interface interfaze, IntrospectedTable introspectedTable) {
+		Method method = new Method("insertOrUpdate");
+		method.setVisibility(JavaVisibility.PUBLIC);
+		method.setReturnType(FullyQualifiedJavaType.getIntInstance());
+		method.setAbstract(true); // 关键：生成抽象方法
 
-		if (element.getName().equals("insert")) {
-			for (Attribute attribute : element.getAttributes()) {
-				if (attribute.getName().equals("id") && attribute.getValue().equals("insertOrUpdate")) {
+		FullyQualifiedJavaType parameterType = introspectedTable.getRules().calculateAllFieldsClass();
+		method.addParameter(new Parameter(parameterType, "record"));
 
-					StringBuilder updateClause = new StringBuilder();
-					updateClause.append("ON DUPLICATE KEY UPDATE ");
+		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
+		interfaze.addMethod(method);
+		return true;
+	}
 
-					List<IntrospectedColumn> columns = ListUtilities
-							.removeIdentityAndGeneratedAlwaysColumns(introspectedTable.getAllColumns());
-					columns.removeAll(introspectedTable.getPrimaryKeyColumns());
-					for (int i = 0; i < columns.size(); i++) {
-						IntrospectedColumn introspectedColumn = columns.get(i);
-						// 过滤不生成update的列
-						if (context.isNotUpdateColumn(introspectedColumn)) {
-							continue;
-						}
+	/**
+	 * 2. 生成 XML SQL
+	 * 修复策略：
+	 * - 使用列名(ColumnName)对比来排除主键，避免对象引用问题。
+	 * - 分段生成 TextElement，确保逻辑清晰，不丢失片段。
+	 */
+	@Override
+	public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+		XmlElement answer = new XmlElement("insert");
+		answer.addAttribute(new Attribute("id", "insertOrUpdate"));
 
-						updateClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
-						updateClause.append("=");
+		FullyQualifiedJavaType parameterType = introspectedTable.getRules().calculateAllFieldsClass();
+		answer.addAttribute(new Attribute("parameterType", parameterType.getFullyQualifiedName()));
 
-						updateClause.append("#{"); //$NON-NLS-1$
-						updateClause.append(introspectedColumn.getJavaProperty());
-						updateClause.append('}');
-						if (i + 1 < columns.size()) {
-							updateClause.append(", "); //$NON-NLS-1$
-						}
-						if (updateClause.length() > 80) {
-							element.addElement(new TextElement(updateClause.toString()));
-							updateClause.setLength(0);
-							OutputUtilities.xmlIndent(updateClause, 1);
-						}
-					}
-					element.addElement(new TextElement(updateClause.toString()));
+		context.getCommentGenerator().addComment(answer);
 
+		List<IntrospectedColumn> allColumns = introspectedTable.getAllColumns();
+		List<IntrospectedColumn> pkColumns = introspectedTable.getPrimaryKeyColumns();
+
+		// --- 第一部分：insert into 表名 (列...) ---
+		StringBuilder insertClause = new StringBuilder();
+		insertClause.append("insert into ");
+		insertClause.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+		insertClause.append(" (");
+
+		Iterator<IntrospectedColumn> iter = allColumns.iterator();
+		while (iter.hasNext()) {
+			IntrospectedColumn col = iter.next();
+			insertClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(col));
+			if (iter.hasNext()) {
+				insertClause.append(", ");
+			}
+		}
+		insertClause.append(")");
+		answer.addElement(new TextElement(insertClause.toString()));
+
+		// --- 第二部分：values (参数...) ---
+		StringBuilder valuesClause = new StringBuilder();
+		valuesClause.append("values (");
+
+		iter = allColumns.iterator();
+		while (iter.hasNext()) {
+			IntrospectedColumn col = iter.next();
+			valuesClause.append(MyBatis3FormattingUtilities.getParameterClause(col));
+			if (iter.hasNext()) {
+				valuesClause.append(", ");
+			}
+		}
+		valuesClause.append(")");
+		answer.addElement(new TextElement(valuesClause.toString()));
+
+		// --- 第三部分：on duplicate key update ... ---
+		// 1. 筛选出非主键的列
+		List<IntrospectedColumn> updateColumns = new ArrayList<>();
+		for (IntrospectedColumn col : allColumns) {
+			boolean isKey = false;
+			for (IntrospectedColumn pk : pkColumns) {
+				// 使用实际列名进行比较，这是最安全的做法
+				if (col.getActualColumnName().equals(pk.getActualColumnName())) {
+					isKey = true;
 					break;
 				}
 			}
+			if (!isKey) {
+				updateColumns.add(col);
+			}
 		}
+
+		// 2. 如果有可更新的列，生成 update 语句
+		if (!updateColumns.isEmpty()) {
+			StringBuilder updateClause = new StringBuilder();
+			updateClause.append("on duplicate key update ");
+
+			Iterator<IntrospectedColumn> updateIter = updateColumns.iterator();
+			while (updateIter.hasNext()) {
+				IntrospectedColumn col = updateIter.next();
+
+				// 格式：column = #{column}
+				updateClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(col));
+				updateClause.append(" = ");
+				updateClause.append(MyBatis3FormattingUtilities.getParameterClause(col));
+
+				if (updateIter.hasNext()) {
+					updateClause.append(", ");
+				}
+			}
+			answer.addElement(new TextElement(updateClause.toString()));
+		}
+
+		document.getRootElement().addElement(answer);
+
 		return true;
 	}
 }
